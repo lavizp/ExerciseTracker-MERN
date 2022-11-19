c275c08416d4f952f239f46384b2ef76b45ef05e
Laviz-pand <pandeylaviz@gmail.com>
2022-11-19 20:10:53 +0545

    Update

--- server/api/dao/restaurantsDAO.js
+++ server/api/dao/restaurantsDAO.js
@@ -1,10 +1,30 @@
 let restaurants
 
 export default class ResturantsDAO{
-    static async injectDB(conn){
-        if(restaurants){
-            return
+    static async injectDB(conn)
+        {
+            if(restaurants){
+                return
+            }
+            try{
+                restaurants = await conn.db(process.env.RESTREVIEWS_NS).collection("restaurants")
+            }
+        catch(e){
+            console.error(
+                `Unable to establish a connection handle in ResturantsDAO: ${e}`
+            )
         }
-        try
+    }
+    static async getRestaurants({
+        filters = null,
+        page = 0,
+        restaurantsPerPage = 20,
+    } = {}){
+        let query
+        if(filters){
+            if("name" in filters){
+                query = {$text: {$search: filters["name"]}}
+            }
+        }
     }
 }

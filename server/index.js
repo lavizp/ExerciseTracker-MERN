4ed0dd75ec17c4272ffaab8a36db447484dcd779
Laviz-pand <pandeylaviz@gmail.com>
2022-11-19 15:31:12 +0545

    Updates

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import app from "./server";
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+
+dotenv.config()
+const MongoClient = mongodb.MongoClient
+
+const port = process.env.PORT || 8000
+
+MongoClient.connect(
+    process.env.RESTREVIEWS_DB_URI,
+    {
+        maxPoolSize: 50,
+        wtimeout: 2500,
+        useNewUrlParse: true,
+
+    }
+).catch(err=>{
+    console.err(err.stack)
+    process.exit(1);
+}).then(async client=>{
+    app.listen(port, ()=>{
+        console.log(`listening on port ${port}`)
+    });
+})

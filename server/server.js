55e4424ee3dc29d124f27e1ac07e6dac23ceaebb
Laviz-pand <pandeylaviz@gmail.com>
2022-12-04 18:35:29 +0545

    Updates

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
 import express  from "express";
 import cors from  "cors";
 import resturants from "./api/resturants.route.js"
-import dotenv from "dotenv"
 
-const port = process.env.PORT || 8000
 const app = express()
 
 app.use(cors())

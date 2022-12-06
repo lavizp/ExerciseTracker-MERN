e3f88a82f2dd8d9733753103319a05b5d7045c90
Laviz-pand <pandeylaviz@gmail.com>
2022-12-06 10:15:40 +0545

    Post Update

--- server/api/gyms.route.js
+++ server/api/gyms.route.js
@@ -5,14 +5,28 @@ let gyms = [
     {
         "name": "Krinagar",
         "location": "Kirtipur",
-        "price": 200
+        "price": 200,
+        "id": 1
     },
     {
         "name": "Titan",
         "location": "Patan",
-        "price": 20000
+        "price": 20000,
+        "id": 2
     }
 ]
-router.route("/").get((req,res)=> res.send("hello world"))
+router.route("/").get((req,res)=> res.send(gyms))
 
+router.route("/:id").get((req,res)=>{
+    let id = req.params.id;
+    res.send(gyms.filter((item)=>{
+        return item.id == id;
+    }))
+})
+
+router.route("/").post((req,res)=>{
+    console.log(req.body)
+    gyms.push(req.body)
+    res.send("D0ne hai ta")
+})
 export default router

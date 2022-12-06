2a1f130418d3b44783bdfe7a11f5cb2f73f702fe
Laviz-pand <pandeylaviz@gmail.com>
2022-12-06 08:49:51 +0545

    updates

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 import app from "./server.js";
 import mongodb from "mongodb"
 import dotenv from "dotenv"
-import bodyParser from 'body-parser'
 import restaurantsDAO from "./dao/restaurantsDAO.js"
 
 dotenv.config()
 const MongoClient = mongodb.MongoClient
-
 const port = process.env.PORT || 8000
 
     app.listen(port, ()=>{
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express  from "express";
 import cors from  "cors";
-import resturants from "./api/resturants.route.js"
+import gymsRoute from "./api/gyms.route.js"
 
 const app = express()
 import bodyParser from 'body-parser'
@@ -8,6 +8,6 @@ import bodyParser from 'body-parser'
 app.use(cors())
 app.use(bodyParser.json())
 
-app.use("/api/v1/resturants", resturants)
+app.use("/gyms", gymsRoute)
 app.use("*", (req, res)=> res.status(404).json({error: "not found"}))
 export default app
